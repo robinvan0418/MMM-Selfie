dc23ecdf1a0c80c3c2581d591559d35848f75e07
Robin Van <robin.van@otto.de>
2018-10-10 20:46:26 +0200

    revert: delay photo telbot message the ugly way

--- MMM-Selfie.js
+++ MMM-Selfie.js
@@ -66,9 +66,8 @@ Module.register('MMM-Selfie',
       }
     	handler.reply('TEXT','Trying to get a selfie...');
     	this.sendSocketNotification('SELFIE', this.config, handler);
-		setTimeout(function(){
-			handler.reply("PHOTO_PATH", "/home/pi/MagicMirror/modules/MMM-Selfie/selfie.jpg", {caption:"Coolest Selfie!"})
-		}, 5000);
+		handler.reply("PHOTO_PATH", "/home/pi/MagicMirror/modules/MMM-Selfie/selfie.jpg", {caption:"Coolest Selfie!"})
+		
 		
  	},
  	cmd_facebook : function (command, handler)
