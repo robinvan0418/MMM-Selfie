3e6ed640c98d25ecb1cb383e6628d7d5001f8b6f
Robin Van <robin.van@otto.de>
2018-10-10 20:44:38 +0200

    delay photo telbot message the ugly way

--- MMM-Selfie.js
+++ MMM-Selfie.js
@@ -40,52 +40,12 @@ Module.register('MMM-Selfie',
         description: this.translate("CMD_TELBOT_SELFIE"),
         callback: 'cmd_selfie'
       })
-      register.add({
-        command: 'facebook',
-        description: this.translate("CMD_TELBOT_FACEBOOK"),
-        callback: 'cmd_facebook'
-      })
-      register.add({
-        command: 'twitter',
-        description: this.translate("CMD_TELBOT_TWITTER"),
-        callback: 'cmd_twitter',
-      })
-      register.add({
-        command: 'tumblr',
-        description: this.translate("CMD_TELBOT_TUMBLR"),
-        callback: 'cmd_tumblr',
-      })
-      register.add({
-        command: 'instagram',
-        description: this.translate("CMD_TELBOT_INSTAGRAM"),
-        callback: 'cmd_instagram',
-      })
     }
     if (register.constructor.name == 'AssistantCommandRegister') {
       register.add({
         command: this.translate("CMD_ASSTNT_SELFIE"),
         description: this.translate("CMD_ASSTNT_SELFIE_DESCRIPTION"),
         callback: 'cmd_selfie'
-      })
-      register.add({
-        command: this.translate("CMD_ASSTNT_FACEBOOK"),
-        description: this.translate("CMD_ASSTNT_FACEBOOK_DESCRIPTION"),
-        callback: 'cmd_facebook',
-      })
-      register.add({
-        command: this.translate("CMD_ASSTNT_TWITTER"),
-        description: this.translate("CMD_ASSTNT_TWITTER_DESCRIPTION"),
-        callback: 'cmd_twitter',
-      })
-      register.add({
-        command: this.translate("CMD_ASSTNT_TUMBLR"),
-        description: this.translate("CMD_ASSTNT_TUMBLR_DESCRIPTION"),
-        callback: 'cmd_tumblr',
-      })
-      register.add({
-        command: this.translate("CMD_ASSTNT_INSTAGRAM"),
-        description: this.translate("CMD_ASSTNT_INSTAGRAM_DESCRIPTION"),
-        callback: 'cmd_instagram',
       })
     }
   },
@@ -106,7 +66,10 @@ Module.register('MMM-Selfie',
       }
     	handler.reply('TEXT','Trying to get a selfie...');
     	this.sendSocketNotification('SELFIE', this.config, handler);
-		handler.reply("PHOTO_PATH", "/home/pi/MagicMirror/modules/MMM-Selfie/selfie.jpg", {caption:"Coolest Picture"})
+		setTimeout(function(){
+			handler.reply("PHOTO_PATH", "/home/pi/MagicMirror/modules/MMM-Selfie/selfie.jpg", {caption:"Coolest Selfie!"})
+		}, 5000);
+		
  	},
  	cmd_facebook : function (command, handler)
 	{
